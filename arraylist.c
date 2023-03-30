19c9571b2fe11eb1806c0a540abb925adcf0c767
Your Name <you@example.com>
2023-03-30 18:05:15 -0300

    new try

--- arraylist.c
+++ arraylist.c
@@ -61,6 +61,11 @@ void push(ArrayList * l, void * data, int i){
 void* pop(ArrayList * l, int i){
     assert(l != NULL);
 
+    if(l->size = 0)
+    {
+        return NULL;
+    }
+
     if(i < 0)
     {
         i = l->size + i;
