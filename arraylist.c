dca8f38eef5c472935c72ef559d92a6974cae6d6
Your Name <you@example.com>
2023-03-30 17:50:27 -0300

    new try

--- arraylist.c
+++ arraylist.c
@@ -49,9 +49,9 @@ void push(ArrayList * l, void * data, int i){
         l->capacity *= 2;
     }
 
-    for (int k = i; k < l->size; k++)
+    for (int k = l->size; k > i; k--)
     {
-        l->data[k + 1] = l->data[k];
+        l->data[k] = l->data[k - 1];
     }
     
     l->data[i] = data;
