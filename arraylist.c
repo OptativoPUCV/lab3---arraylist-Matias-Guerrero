ec0e9f9d3bfb269b6ec472c426f44781f8fb0d09
Your Name <you@example.com>
2023-03-30 17:46:00 -0300

    new try

--- arraylist.c
+++ arraylist.c
@@ -11,15 +11,51 @@ typedef struct ArrayList {
 } ArrayList;
 
 ArrayList *createList(void) {
+    ArrayList* nuevo = (ArrayList *)malloc(sizeof(ArrayList));
+
+    assert(nuevo != NULL);
+
+    nuevo->data = malloc(2 * sizeof(void*));
+    nuevo->capacity = 2;
+    nuevo->size = 0;
+
     return NULL;
 }
 
 void append(ArrayList * l, void * data){
+    assert(l != NULL);
 
+    if(l->size == l->capacity)
+    {
+        l->data = realloc(l->data, (l->capacity * 2) * sizeof(void*));
+        l->capacity *= 2;
+    }
+
+    l->data[l->size] = data;
+    l->size++;
 }
 
 void push(ArrayList * l, void * data, int i){
+    assert(l != NULL);
 
+    if(i < 0 || i > l->size)
+    {
+        return;
+    }
+
+    if(l->size == l->capacity)
+    {
+        l->data = realloc(l->data, (l->capacity * 2) * sizeof(void*));
+        l->capacity *= 2;
+    }
+
+    for (int k = i; k < l->size; k++)
+    {
+        l->data[k + 1] = l->data[k];
+    }
+    
+    l->data[i] = data;
+    l->size++;
 }
 
 void* pop(ArrayList * l, int i){
